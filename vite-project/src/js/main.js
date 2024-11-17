3e4ddb90ea6deab70d77eac93cd4ff129d62ae28
OKuzminova <yagodka029@yandex.ru>
2024-11-17 21:52:25 +0300

    Run vite

---
+++ vite-project/src/js/main.js
@@ -0,0 +1,21 @@
+import '../less/style.less';
+
+// const navTag = document.querySelector(".nav");
+// const btnBurger = document.querySelector(".menu__button");
+// const width = window.innerWidth;
+
+// const menuChild = document.querySelector(".menu-nav__child");
+// const linkActive = document.querySelector(".nav__active");
+
+// if (width <= 768) {
+//     navTag.classList.add("visually-hidden");
+//     btnBurger.classList.remove("visually-hidden");
+//     menuChild.classList.remove("visually-hidden");
+//     } else {
+//         navTag.classList.remove("visually-hidden");
+//         btnBurger.classList.add("visually-hidden"); 
+//         menuChild.classList.remove("visually-hidden");
+// };
+// linkActive.addEventListener('click', function (event) { 
+//     menuChild.classList.remove("visually-hidden");
+// });
